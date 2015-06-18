55be46e6c349c67de01584320163ad3f87f9387b
Tyson Bailey <onaclov2000+github@gmail.com>
2015-06-18 14:33:10 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+app.all('*', function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "X-Requested-With");
+next();
+});
+app.post('/ReceiveJSON', function(req, res){
+console.log(req.body);
+
+res.send("ok");
+});
+ 
+app.listen(3000);
+console.log('listening to http://localhost:3000'); 
